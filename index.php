0ce43746fa3af979e1be0e9c3132611dcb3fc633
tbence94 <tihanyibence@gmail.com>
2017-01-24 11:46:03 +0000

    Test a little less for faster results.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once 'vendor/autoload.php';
 
 use App\Analyzer;
 
-foreach ([10, 100, 1000, 10000] as $amount) {
+foreach ([10, 100, 1000] as $amount) {
     $analyzer = new Analyzer($amount);
     $analyzer->analyzeAlgorithms();
 }
